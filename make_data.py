e8cee2a47d9a4f3db956499805d1f77273990c7d
KFC-Peat <cdp.caseypeat@gmail.com>
2017-06-27 03:16:34 +1200

    Make numpy arrays

--- make_data.py
+++ make_data.py
@@ -1,7 +1,47 @@
 import numpy as np
 import scipy as sp
+from scipy import misc
 from os import listdir
+import sys
+import matplotlib.pyplot as plt
+import csv
 
-train_list = listdir('../train')
 
-print(train_list)
+### MAKE THE CSV DATA INTO NUMPY ARRAY
+IMG_NUM = 2295
+
+data_labels = np.zeros([IMG_NUM], dtype=np.uint8)
+
+with open('../train_labels.csv', 'r') as f:
+    csv_object = csv.reader(f)
+    csv_list = []
+    for line in csv_object:
+        csv_list.append(line)
+
+for i in range(IMG_NUM):
+    data_labels[i] = int(csv_list[i+1][1])
+
+with open('./data/labels.npy', 'wb') as f:
+    np.save(f, data_labels)
+
+
+
+### MAKE THE IMAGE DATA INTO NUMPY ARRAY
+IMG_NUM = 2295
+IMG_SIZE = 32
+
+data_set = np.zeros([IMG_NUM,IMG_SIZE,IMG_SIZE,3], dtype=np.uint8)
+
+for i in range(IMG_NUM):
+
+    fp = '../train/{}.jpg'.format(i+1)
+
+    image = sp.misc.imread(fp)
+    image_scaled = sp.misc.imresize(image,[32,32,3])
+    data_set[i,:,:,:] = image_scaled
+
+    if i%100 == 0:
+        print(i)
+
+with open('./data/image32.npy', 'wb') as f:
+    np.save(f, data_set)
